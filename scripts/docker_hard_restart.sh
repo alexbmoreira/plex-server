56d658f64bee733c0d9faddcf725f0e9cb3c5e48
Alex <alexbmoreira@gmail.com>
2025-04-06 11:31:16 -0400

    some improvements

--- scripts/docker_hard_restart.sh
+++ scripts/docker_hard_restart.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker ps -a --format "{{.ID}} {{.Names}}" | grep -v -e "plex" | awk "{print \$2}" | xargs docker stop
-docker ps -a --format "{{.ID}} {{.Names}}" | grep -v -e "plex" | awk "{print \$2}" | xargs docker rm
-cd ~/plex/docker && docker compose up -d
+docker ps -a --format "{{.ID}} {{.Names}}" | grep -v -e "plex" | awk "{print \$2}" | xargs docker compose down -f $PLEX/docker/docker-compose.yml
+docker compose up -d -f $PLEX/docker/docker-compose.yml
