1e9541d8fe207d04c1e31ba6235e2eb98ba1b91a
Alex <alexbmoreira@gmail.com>
2025-06-05 16:40:21 -0400

    Kometa changes

--- scripts/kometa_run.sh
+++ scripts/kometa_run.sh
@@ -3,6 +3,6 @@
 source $PLEX/docker/.env
 
 docker run --rm -it -v "/home/$USER/plex/kometa/config:/config" kometateam/kometa:nightly --kometa-IP_ADDRESS $IP_ADDRESS \
-  --kometa-PLEX_TOKEN $PLEX_TOKEN \
-  --kometa-TMDB_API_KEY $TMDB_API_KEY \
-  --kometa-RADARR_API_KEY $RADARR_API_KEY --run
+  --kometa-PLEXTOKEN $PLEX_TOKEN \
+  --kometa-TMDBKEY $TMDB_API_KEY \
+  --kometa-RADARRKEY $RADARR_API_KEY --run
