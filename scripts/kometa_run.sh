58106cc2ecdc66bbe48426d9a9e478a780841692
Alex Moreira <alexbmoreira@gmail.com>
2025-11-13 17:31:04 -0500

    Kometa collection sorting (#7)
    * Improve Kometa collections
    
    * ??

--- scripts/kometa_run.sh
+++ scripts/kometa_run.sh
@@ -2,7 +2,7 @@
 
 source $PLEX/docker/.env
 
-docker run --rm -it -v "/home/$USER/plex/kometa/config:/config" kometateam/kometa:nightly --kometa-IP_ADDRESS $IP_ADDRESS \
+docker run --rm -it -v "/home/$USER/plex/kometa/config:/config" kometateam/kometa:nightly --kometa-IPADDRESS $IP_ADDRESS \
   --kometa-PLEXTOKEN $PLEX_TOKEN \
   --kometa-TMDBKEY $TMDB_API_KEY \
   --kometa-RADARRKEY $RADARR_API_KEY --run
